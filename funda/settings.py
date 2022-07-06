739bde1b3415ee9e17763988e209696ac50f21eb
Raphael <raphaelsparenberg@kateinnovations.com>
2022-07-06 08:49:21 +0200

    fix sold

--- funda/settings.py
+++ funda/settings.py
@@ -18,7 +18,7 @@ NEWSPIDER_MODULE = 'funda.spiders'
 # USER_AGENT = 'funda (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
